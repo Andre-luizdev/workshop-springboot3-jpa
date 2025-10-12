092ac68e7940c0038403ec5457ae183adb3e07ff
Andre-luizdev <129134069+Andre-luizdev@users.noreply.github.com>
2025-10-12 09:04:40 -0300

    Exception handling - findById

---
+++ src/main/java/com/andredev/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.andredev.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id" + id);
+    }
+
+}
